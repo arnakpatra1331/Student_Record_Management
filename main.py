88a7f6385d9b6720dc43cc9c8d3073aa453776ba
Arnak Patra <arnak1331@gmail.com>
2026-09-09 11:14:29 +0530

    FInal

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def main():
     parser.add_argument("--search-dept", help="Search student by Department")
     parser.add_argument("--min-avg", type=float, help="Search students with average marks >= min-avg")
     
-    parser.add_argument("--add", nargs=6, metavar=('ID', 'NAME', 'DEPT', 'SEM', 'M1', 'M2_M3'),
-                        help="Add new student: ID Name Dept Sem Mark1 Mark2 Mark3")
+    parser.add_argument("--add", nargs=7, metavar=('ID', 'NAME', 'DEPT', 'SEM', 'M1', 'M2', 'M3'),
+                        help="Add new student: ID Name Dept Sem Mark1 Mark2 Mark3 (e.g. 406 Arnak CSE 1 80 85 90)")
     parser.add_argument("--out", help="Output file path to save updated records")
 
     args = parser.parse_args()
